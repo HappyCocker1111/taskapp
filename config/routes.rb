b74c6c985d0bb4e85ab6f062bf8afa04fd86918f
Miwa <a.cokegasuki@gmail.com>
2017-01-31 08:08:56 +0000

    Login Logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+
   root 'welcome#index'
   get 'welcome/index'
+  
+  get '/login', to:'sessions#new'
+  post '/login', to:'sessions#create'
+  delete '/logout', to:'sessions#destroy'
   
   resources :products
   resources :diaries
